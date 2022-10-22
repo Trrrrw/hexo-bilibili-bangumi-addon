12ca76f2f6562268edb35a6a9642615af707f32d
Trrrrw <wzhhenry@qq.com>
2022-10-22 21:17:55 +0800

    把每次获取到的数据存在本地，这样就不用每次都获取一样的数据了。

--- README.md
+++ README.md
@@ -1,5 +1,20 @@
+<<<<<<< Updated upstream
 # hexo-bilibili-bangumi-addon
 A python script for hexo-bilibili-bangumi  
 自己的博客在用这个插件：[hexo-bilibili-bangumi](https://github.com/HCLonely/hexo-bilibili-bangumi)。但是用的bangumi源不能获取除标题和封面之外的信息。  
 不会写Node.js，所以就写了个Python来补全番剧的信息。  
 > 准备把每次获取到的数据存在本地，这样就不用每次都获取一样的数据了。
+=======
+# hexo-bilibili-bangumi-addon
+A python script for hexo-bilibili-bangumi  
+自己的博客在用这个插件：[hexo-bilibili-bangumi](https://github.com/HCLonely/hexo-bilibili-bangumi)。但是用的bangumi源不能获取除标题和封面之外的信息。  
+不会写Node.js，所以就写了个Python来补全番剧的信息。  
+22/10/22：随便写了点，现在运行之后会在`sourse/_data`下多出一个`bangumis-save.json`，这个文件内容和`bangumis.json`一样，每次更新的时候会先从这个文件里面找。如果想要重新从api获取的话把`bangumis-save.json`删掉就行了
+
+# 使用
+1. 给你的网站安装这个插件[hexo-bilibili-bangumi](https://github.com/HCLonely/hexo-bilibili-bangumi)，并根据其说明做好配置；
+2. 在网页根目录运行一次`hexo bangumi -u`确保插件配置正常；
+3. 安装[Python](https://www.python.org/)；
+4. 安装`httpx`：`pip install httpx`；
+5. 将`main.py`放到网页根目录，在终端中运行：`python main.py`
+>>>>>>> Stashed changes
--- main.py
+++ main.py
@@ -1,5 +1,111 @@
 import httpx
 import json
+import os
+
+
+def local_file(id, bangumis):
+    if os.path.exists('./source/_data/bangumis-save.json'):
+        with open("./source/_data/bangumis-save.json", "r", encoding="utf-8") as js_file:
+            js_txt = js_file.read()
+            py_data = json.loads(js_txt)
+            wantWatch = py_data.get("wantWatch")
+            watching = py_data.get("watching")
+            watched = py_data.get("watched")
+            for i in wantWatch:
+                if i["id"] == id:
+                    try:
+                        bangumis.score = i['score']
+                    except:
+                        bangumis.score = "-"
+                    try:
+                        bangumis.des = i['des']
+                    except:
+                        bangumis.des = "-"
+                    try:
+                        bangumis.wish = i['wish']
+                    except:
+                        bangumis.wish = "-"
+                    try:
+                        bangumis.doing = i['doing']
+                    except:
+                        bangumis.doing = "-"
+                    try:
+                        bangumis.collect = i['collect']
+                    except:
+                        bangumis.collect = "-"
+
+                    try:
+                        bangumis.totalCount = i['totalCount']
+                    except:
+                        bangumis.totalCount = "全12话"
+                    return bangumis
+                else:
+                    continue
+            for i in watching:
+                if i["id"] == id:
+                    try:
+                        bangumis.score = i['score']
+                    except:
+                        bangumis.score = "-"
+                    try:
+                        bangumis.des = i['des']
+                    except:
+                        bangumis.des = "-"
+                    try:
+                        bangumis.wish = i['wish']
+                    except:
+                        bangumis.wish = "-"
+                    try:
+                        bangumis.doing = i['doing']
+                    except:
+                        bangumis.doing = "-"
+                    try:
+                        bangumis.collect = i['collect']
+                    except:
+                        bangumis.collect = "-"
+
+                    try:
+                        bangumis.totalCount = i['totalCount']
+                    except:
+                        bangumis.totalCount = "全12话"
+                    return bangumis
+                else:
+                    continue
+            for i in watched:
+                if i["id"] == id:
+                    try:
+                        bangumis.score = i['score']
+                    except:
+                        bangumis.score = "-"
+                    try:
+                        bangumis.des = i['des']
+                    except:
+                        bangumis.des = "-"
+                    try:
+                        bangumis.wish = i['wish']
+                    except:
+                        bangumis.wish = "-"
+                    try:
+                        bangumis.doing = i['doing']
+                    except:
+                        bangumis.doing = "-"
+                    try:
+                        bangumis.collect = i['collect']
+                    except:
+                        bangumis.collect = "-"
+
+                    try:
+                        bangumis.totalCount = i['totalCount']
+                    except:
+                        bangumis.totalCount = "全12话"
+                    return bangumis
+                else:
+                    continue
+    else:
+        with open('./source/_data/bangumis-save.json', 'x', encoding="utf-8") as new:
+            new.write('{"wantWatch":[],"watching":[],"watched":[]}')
+    return 0
+
 
 def get_data(id):
     class bangumis:
@@ -10,7 +116,11 @@ def get_data(id):
         collect = "-"
         totalCount = "-"
 
-    url = "https://api.bgm.tv/v0/subjects/"+id
+    local_data = local_file(id, bangumis)
+    if local_data != 0:
+        return local_data
+
+    url = "https://api.bgm.tv/v0/subjects/" + id
     headers = {
         'user-agent': 'Trrrrw/hexo-bilibili-bangumi-addon(https://github.com/Trrrrw/hexo-bilibili-bangumi-addon)',
         'accept': 'application / json'
@@ -52,8 +162,20 @@ def read():
     with open("./source/_data/bangumis.json", "r", encoding="utf-8") as js_file:
         js_txt = js_file.read()
     py_data = json.loads(js_txt)
+    wantWatch = py_data.get("wantWatch")
     watching = py_data.get("watching")
     watched = py_data.get("watched")
+    for k in wantWatch:
+        k["type"] = "番剧"
+        print("\n正在获取 " + k["title"] + " 的信息")
+        bangumis = get_data(k["id"])
+        k["score"] = bangumis.score
+        k["des"] = bangumis.des
+        k["wish"] = bangumis.wish
+        k["doing"] = bangumis.doing
+        k["collect"] = bangumis.collect
+        k["totalCount"] = "全" + str(bangumis.totalCount) + "话"
+
     for i in watching:
         i["type"] = "番剧"
         print("\n正在获取 " + i["title"] + " 的信息")
@@ -84,8 +206,12 @@ def write(py_data):
     with open("./source/_data/bangumis.json", "w", encoding="utf-8") as js_output:
         js_output_txt = json.dumps(py_data, ensure_ascii=False)
         js_output.write(js_output_txt)
+    with open("./source/_data/bangumis-save.json", "w", encoding="utf-8") as js_output:
+        js_output_txt = json.dumps(py_data, ensure_ascii=False)
+        js_output.write(js_output_txt)
 
 
 if __name__ == "__main__":
+    os.system("hexo bangumi -u")
     write(read())
     print("\n完成")
